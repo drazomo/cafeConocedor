6a77705ea1969044574db86109cd6feff3217899
Mark Rasavong <mark.rasavong@gmail.com>
2022-03-12 08:35:10 +0100

    true fallback && map json to getStaticPaths

--- pages/cafeteria/[id].tsx
+++ pages/cafeteria/[id].tsx
@@ -1,12 +1,22 @@
-import type { GetStaticProps, NextPage } from 'next'
+import type { GetStaticPathsResult, GetStaticProps, NextPage } from 'next'
+import Head from 'next/head';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { ICafeterias } from '..';
 import cafeteriaData from '../../data/cafeterias.json';
 
-const Cafeteria: NextPage = () => {
+const Cafeteria = (props: ICafeterias) => {
   const router = useRouter();
+  // fallback version if page is rendered for first time
+  {router.isFallback && <div>Loading...</div>}
+  //destructuring happpens incase of the router fallback / rendering data for the 1st time
+  const { address, name, zone } = props;
+
   return (
     <div>
+      <Head>
+        <title>{name}</title>
+      </Head>
       Cafeteria
       <Link href='/'>
         <a>Volver</a>
@@ -14,22 +24,38 @@ const Cafeteria: NextPage = () => {
       <Link href='/cafeteria/dynamic'>
         <a>Go to dynamic page</a>
       </Link>
+      <p>{address}</p>
+      <p>{name}</p>
+      <p>{zone}</p>
     </div>
   )
 };
 
 export const getStaticProps: GetStaticProps = (context) => {
-  const params = context.params;
-  console.log(params);
+  // signifies ! that the params will not be undefined && str converted to number for type capability
+  const paramsId = context.params!.id;
+  const findCafteriaById = cafeteriaData.find(local => local.id.toString() === paramsId)
+  console.log(findCafteriaById);
   return {props: {
-    cafeteria: cafeteriaData.find(local => (local.id === 0))
+    cafeteria: findCafteriaById
   }}
+
 };
 
-export const getStaticPaths = () => {
+export const getStaticPaths = async({}): Promise<GetStaticPathsResult> => {
+  // fallback false => 404 error 
+  // fallback true => best if you have a lot of static pages
+  // if fallback is true it will download it's content then will be cached for the next user in the cdn
+  const paths = cafeteriaData.map(cafe => {
+    return {
+      params: {
+        id: cafe.id.toString(),
+      }
+    }
+  });
   return {
-    paths: [{ params : { id: '0' }}, { params: { id: '1' }}],
-    fallback: false
+    paths,
+    fallback: true
   }
 }
 
